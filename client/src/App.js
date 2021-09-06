553a527eedec75e2cab1df7217176993b8dd3651
kmulqueen <kmulqdrums710@gmail.com>
2021-09-06 14:51:22 -0600

    Added socket.io basic connection.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+import io from "socket.io-client";
 import Board from "./features/board/Board";
 
 function App() {
+  const socket = io("http://localhost:5000/");
+
   return (
     <>
       <Board />
--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 const PORT = 5000 || process.env.PORT;
+
+io.on("connection", (socket) => {
+  console.log("socket.io: New user connected =>", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("socket.io: User disconnected =>", socket.id);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
